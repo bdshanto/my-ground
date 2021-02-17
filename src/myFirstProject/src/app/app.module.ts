7accf16ffbbd876450ba381cd1eb347df09b277f
Hasibul Islam SHaanTO <bdshanto@hotmail.com>
2021-02-17 21:52:04 +0600

    product component created

--- src/myFirstProject/src/app/app.module.ts
+++ src/myFirstProject/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
