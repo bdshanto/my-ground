bb43a983c223155b75fd554486bf6f474ddede2a
Hasibul Islam Shanto <bdshanto@hotmail.com>
2021-03-05 07:49:53 +0600

    material module added

--- src/fitness-tracker/src/app/app.module.ts
+++ src/fitness-tracker/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { MaterialModule } from './material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
